7cac022527e0c2fb951eb49f65b52e44f5537e41
boatman223 <jmquander@gmail.com>
2019-12-07 20:26:36 -0500

    cleaner

--- day7/7-2.py
+++ day7/7-2.py
@@ -1,126 +1,126 @@
 import itertools
 
-def get_parameter_count(opcode):
-    count = {
-        '01': 3,
-        '02': 3,
-        '03': 1,
-        '04': 1,
-        '05': 2,
-        '06': 2,
-        '07': 3,
-        '08': 3,
-        '99': 0
-    }
-    return count[opcode]
+class Computer:
 
-def opcode_handlers():
-    handlers = {
-        '01': op_ADD,
-        '02': op_MUL,
-        '03': op_INP,
-        '04': op_OUT,
-        '05': op_JIT,
-        '06': op_JIF,
-        '07': op_TLT,
-        '08': op_TEQ,
-        '99': op_TRM
-    }
-    return handlers
+    RUNNING = 0
+    PAUSED  = 1
+    HALTED  = 2
 
-def set_parameter(code, parameter, mode):
-    if mode == '0': return code[parameter]
-    elif mode == '1': return parameter
+    def __init__(self, code, pc=0, status=0, stdin=0):
+        self.code = code
+        self.pc = pc
+        self.status = status
+        self.stdin = stdin
+        self.stdout = 0
+        self.execute()
 
-def op_ADD(code, index, parameters, phase, inputval):
-    code[code[index-1]] = parameters[0] + parameters[1]
-    return code, index, phase, 0
+    def send_input(self, stdin):
+        self.stdin = stdin
+        self.op_INP([self.code[self.pc-1]], '')
+        self.execute()
 
-def op_MUL(code, index, parameters, phase, inputval):
-    code[code[index-1]] = parameters[0] * parameters[1]
-    return code, index, phase, 0
+    def get_param_count(self, opcode):
+        count = {
+            '01': 3,
+            '02': 3,
+            '03': 1,
+            '04': 1,
+            '05': 2,
+            '06': 2,
+            '07': 3,
+            '08': 3,
+            '99': 0
+        }
+        return count[opcode]
 
-def op_INP(code, index, parameters, phase, inputval):
-    if phase != 999:
-        code[code[index-1]] = phase
-        phase = 999
-    else:
-        code[code[index-1]] = inputval
-    return code, index, phase, 0
+    def opcode_handlers(self):
+        handlers = {
+            '01': self.op_ADD,
+            '02': self.op_MUL,
+            '03': self.op_INP,
+            '04': self.op_OUT,
+            '05': self.op_JIT,
+            '06': self.op_JIF,
+            '07': self.op_TLT,
+            '08': self.op_TEQ,
+            '99': self.op_TRM
+        }
+        return handlers
 
-def op_OUT(code, index, parameters, phase, inputval):
-    phase = parameters[0]
-    print(f'output: {parameters[0]}')
-    return code, index, phase, 1
+    def set_param(self, param, mode):
+        if mode == '0': return self.code[param]
+        elif mode == '1': return param
 
-def op_JIT(code, index, parameters, phase, inputval):
-    if parameters[0]: index = parameters[1]
-    return code, index, phase, 0
+    def op_ADD(self, params, modes):
+        self.code[params[2]] = self.set_param(params[0], modes[0]) + self.set_param(params[1], modes[1])
 
-def op_JIF(code, index, parameters, phase, inputval):
-    if not parameters[0]: index = parameters[1]
-    return code, index, phase, 0
+    def op_MUL(self, params, modes):
+        self.code[params[2]] = self.set_param(params[0], modes[0]) * self.set_param(params[1], modes[1])
 
-def op_TLT(code, index, parameters, phase, inputval):
-    if parameters[0] < parameters[1]: code[code[index-1]] = 1
-    else: code[code[index-1]] = 0
-    return code, index, phase, 0
+    def op_INP(self, params, modes):
+        if self.status == self.RUNNING:
+            self.status = self.PAUSED
+        elif self.status == self.PAUSED:
+            self.status = self.RUNNING
+            self.code[params[0]] = self.stdin
 
-def op_TEQ(code, index, parameters, phase, inputval):
-    if parameters[0] == parameters[1]: code[code[index-1]] = 1
-    else: code[code[index-1]] = 0
-    return code, index, phase, 0
+    def op_OUT(self, params, modes):
+        self.stdout = self.set_param(params[0], modes[0])
+        print(f'output: {self.stdout}')
 
-def op_TRM(code, index, parameters, phase, inputval):
-    return code, index, phase, 2
+    def op_JIT(self, params, modes):
+        if self.set_param(params[0], modes[0]):
+            self.pc = self.set_param(params[1], modes[1])
 
-def run_computer(code, index, phase, inputval):
-    done = 0
-    while not done:
-        opcode = f'{code[index]:0>5}'[-2:]
-        modes = f'{code[index]:0>5}'[0:3][::-1]
-        increment = get_parameter_count(opcode) + 1
-        parameters = code[index+1:index+increment]
-        index += increment
-        for i, parameter in enumerate(parameters[:]):
-            mode = modes[i]
-            parameters[i] = set_parameter(code, parameter, mode)
-        code, index, phase, done = opcode_handlers()[opcode](code, index, parameters, phase, inputval)
-    return code, phase, index, done
+    def op_JIF(self, params, modes):
+        if not self.set_param(params[0], modes[0]):
+            self.pc = self.set_param(params[1], modes[1])
+
+    def op_TLT(self, params, modes):
+        if self.set_param(params[0], modes[0]) < self.set_param(params[1], modes[1]):
+            self.code[params[2]] = 1
+        else:
+            self.code[params[2]] = 0
+
+    def op_TEQ(self, params, modes):
+        if self.set_param(params[0], modes[0]) == self.set_param(params[1], modes[1]):
+            self.code[params[2]] = 1
+        else:
+            self.code[params[2]] = 0
+
+    def op_TRM(self, params, modes):
+        self.status = self.HALTED
+
+    def execute(self):
+        while self.status == self.RUNNING:
+            opcode = f'{self.code[self.pc]:0>5}'[-2:]
+            modes = f'{self.code[self.pc]:0>5}'[0:3][::-1]
+            increment = self.get_param_count(opcode) + 1
+            params = self.code[self.pc+1:self.pc+increment]
+            self.pc += increment
+            self.opcode_handlers()[opcode](params, modes)
+        return
 
 with open('input.txt','r') as fh:
     basecode = [int(i) for i in fh.read().strip('\n').split(',')]
 
-tests = list(itertools.permutations([5,6,7,8,9]))
-
-
+tests = list(itertools.permutations(range(5,10)))
 
 highest_output = 0
 for test in tests:
-    done = 1
-    amps = {
-        0: [basecode[:], 0, test[0], 0],
-        1: [basecode[:], 0, test[1], 0],
-        2: [basecode[:], 0, test[2], 0],
-        3: [basecode[:], 0, test[3], 0],
-        4: [basecode[:], 0, test[4], 0]
-    }
-    i = 0
-    done = 1
-    while done == 1:
-        code, output, index, done = run_computer(amps[i][0], amps[i][1], amps[i][2], amps[i][3])
-        amps[i][0] = code
-        amps[i][1] = index
-        amps[i][2] = 999
-        try:
-            amps[i+1][3] = output
-        except:
-            amps[0][3] = output
-            i = 0
-        else:
-            i += 1
+    amps = []
+    for i in range(0, 5):
+        amp = Computer(basecode)
+        amps.append(amp)
+        amp.send_input(test[i])
+    ampcycle = itertools.cycle(amps)
+    output = 0
+    while amps[4].status != Computer.HALTED:
+        running = next(ampcycle)
+        running.send_input(output)
+        output = running.stdout
 
-    if amps[0][3] > highest_output:
-        highest_output = amps[0][3]
+    if amps[4].stdout > highest_output:
+        highest_output = amps[4].stdout
 
 print(highest_output)
