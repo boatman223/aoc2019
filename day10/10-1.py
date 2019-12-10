c42327e9bbdf4bcc12634ac1b193d6b58faa434d
boatman223 <jmquander@gmail.com>
2019-12-10 02:04:44 -0500

    less stupid

--- day10/10-1.py
+++ day10/10-1.py
@@ -1,6 +1,7 @@
+import math
+
 with open('input.txt', 'r') as fh:
     data = [line.strip() for line in fh]
-    print(data)
 
 asteroids = set()
 for i, line in enumerate(data):
@@ -11,23 +12,11 @@ for i, line in enumerate(data):
 most = 0
 for a1 in asteroids:
     slopes = set()
-    slopes2 = set()
     for a2 in asteroids:
         if a1 == a2: continue
-        try:
-            slope = (a2[1]-a1[1]) / (a2[0]-a1[0])
-        except ZeroDivisionError:
-            slope = 999
-        if a2[1] > a1[1]:
-            slopes.add(slope)
-        elif a2[1] < a1[1]:
-            slopes2.add(slope)
-        else:
-            if a2[0] > a1[0]:
-                slopes.add(slope)
-            else:
-                slopes2.add(slope)
-    if len(slopes)+len(slopes2) > most:
-        most = len(slopes)+len(slopes2)
+        angle = math.atan2(a2[1]-a1[1], a2[0]-a1[0])
+        slopes.add(angle)
+    if len(slopes) > most:
+        most = len(slopes)
 
 print(most)
