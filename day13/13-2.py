6b1c84b4a3f3daf1d5b57b5e0e6ec99b1a7804ed
boatman223 <jmquander@gmail.com>
2019-12-13 02:45:39 -0500

    13-2

---
+++ day13/13-2.py
@@ -0,0 +1,167 @@
+import itertools
+import collections
+
+class Computer:
+
+    RUNNING = 0
+    PAUSED  = 1
+    HALTED  = 2
+
+    def __init__(self, code, pc=0, stdin=None):
+        self.code = code
+        self.pc = pc
+        self.rbase = 0
+        self.status = self.RUNNING
+        self.stdin = stdin
+        self.stdout = []
+        self.execute()
+
+    def send_input(self, stdin):
+        self.stdin = stdin
+        self.status = self.RUNNING
+        self.pc -= 2
+        self.execute()
+
+    def get_output(self):
+        output = self.stdout
+        self.stdout = []
+        return output
+
+    def get_param_count(self, opcode):
+        count = {
+            '01': 3,
+            '02': 3,
+            '03': 1,
+            '04': 1,
+            '05': 2,
+            '06': 2,
+            '07': 3,
+            '08': 3,
+            '09': 1,
+            '99': 0
+        }
+        return count[opcode]
+
+    def opcode_handlers(self):
+        handlers = {
+            '01': self.op_ADD,
+            '02': self.op_MUL,
+            '03': self.op_INP,
+            '04': self.op_OUT,
+            '05': self.op_JIT,
+            '06': self.op_JIF,
+            '07': self.op_TLT,
+            '08': self.op_TEQ,
+            '09': self.op_RBO,
+            '99': self.op_TRM
+        }
+        return handlers
+
+    def set_param(self, param):
+        if param[1] == '0': return self.code[param[0]]
+        elif param[1] == '1': return param[0]
+        elif param[1] == '2': return self.code[param[0]+self.rbase]
+
+    def set_param_w(self, param):
+        if param[1] == '0': return param[0]
+        elif param[1] == '2': return param[0]+self.rbase
+
+    def op_ADD(self, params):
+        self.code[self.set_param_w(params[2])] = self.set_param(params[0]) + self.set_param(params[1])
+
+    def op_MUL(self, params):
+        self.code[self.set_param_w(params[2])] = self.set_param(params[0]) * self.set_param(params[1])
+
+    def op_INP(self, params):
+        if self.stdin == None:
+            self.status = self.PAUSED
+        else:
+            self.code[self.set_param_w(params[0])] = self.stdin
+            self.stdin = None
+
+    def op_OUT(self, params):
+        self.stdout.append(self.set_param(params[0]))
+        #print(f'output: {self.stdout}')
+
+    def op_JIT(self, params):
+        if self.set_param(params[0]):
+            self.pc = self.set_param(params[1])
+
+    def op_JIF(self, params):
+        if not self.set_param(params[0]):
+            self.pc = self.set_param(params[1])
+
+    def op_TLT(self, params):
+        if self.set_param(params[0]) < self.set_param(params[1]):
+            self.code[self.set_param_w(params[2])] = 1
+        else:
+            self.code[self.set_param_w(params[2])] = 0
+
+    def op_TEQ(self, params):
+        if self.set_param(params[0]) == self.set_param(params[1]):
+            self.code[self.set_param_w(params[2])] = 1
+        else:
+            self.code[self.set_param_w(params[2])] = 0
+
+    def op_RBO(self, params):
+        self.rbase += self.set_param(params[0])
+
+    def op_TRM(self, params):
+        self.status = self.HALTED
+
+    def execute(self):
+        while self.status == self.RUNNING:
+            opcode = f'{self.code[self.pc]:0>5}'[-2:]
+            modes = f'{self.code[self.pc]:0>5}'[0:3][::-1]
+            increment = self.get_param_count(opcode) + 1
+            params = list(zip(self.code[self.pc+1:self.pc+increment], list(modes)))
+            self.pc += increment
+            self.opcode_handlers()[opcode](params)
+        return
+
+def print_image():
+    xvalues = [i[0] for i in tiles.keys()]
+    yvalues = [i[1] for i in tiles.keys()]
+    for i in range(min(yvalues), max(yvalues)+1):
+        line = ''
+        for j in range(min(xvalues), max(xvalues)+1):
+            if (j, i) == (-1, 0): line += f'{str(tiles[(j,i)])}\n'
+            elif j == -1: continue
+            elif tiles[(j,i)] == 0: line += '  '
+            elif tiles[(j,i)] == 1: line += '..'
+            elif tiles[(j,i)] == 2: line += '[]'
+            elif tiles[(j,i)] == 3: line += "=="
+            elif tiles[(j,i)] == 4: line += '()'
+        print(line)
+
+def timestep(ball, paddle):
+    output = game.get_output()
+    for i in range(0, len(output), 3):
+        if output[i+2] == 3: paddle = output[i]
+        if output[i+2] == 4: ball = output[i]
+        tiles[(output[i], output[i+1])] = output[i+2]
+    return ball, paddle
+
+with open('input.txt','r') as fh:
+    basecode = [int(i) for i in fh.read().strip('\n').split(',')]
+    basecode.extend([0]*10000)
+    basecode[0] = 2
+
+tiles = collections.defaultdict(int)
+game = Computer(basecode[:])
+ball = 0
+paddle = 0
+steps = 0
+while game.status != game.HALTED:
+    steps += 1
+    ball, paddle = timestep(ball, paddle)
+    if ball > paddle:
+        game.send_input(1)
+    elif paddle > ball:
+        game.send_input(-1)
+    else:
+        game.send_input(0)
+    if steps % 50 == 0: print_image()
+
+timestep(ball, paddle)
+print_image()
