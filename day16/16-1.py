802271acd7de978b2b9a221493a2d86479d876a2
boatman223 <jmquander@gmail.com>
2019-12-17 16:10:24 -0500

    16-1

--- day16/16-1.py
+++ day16/16-1.py
@@ -6,13 +6,12 @@ with open('input.txt', 'r') as fh:
 basepattern = [[0], [1], [0], [-1]]
 for i in range(1, 101):
     newdata = []
-    for j, k in enumerate(data):
+    for j in range(len(data)):
         pattern = []
-        for l in basepattern:
-            pattern.extend((j+1)*l)
+        for k in basepattern:
+            pattern.extend((j+1)*k)
         pattern = itertools.cycle(pattern[:])
         next(pattern)
-
         digit = abs(sum(map(lambda x, y: x * y, data, pattern)))
         newdata.append(int(str(digit)[-1]))
     data = newdata[:]
